2bf996f8a9e76de7ed6d33323c83dda2add2e1e7
VivianaHarrizonM <viviana.harrizon@gmail.com>
2026-08-05 18:44:05 -0600

    add homework seven

--- FundamentosCsharp-Seccion8/Seccion9/Tarea9.cs
+++ FundamentosCsharp-Seccion8/Seccion9/Tarea9.cs
@@ -4,6 +4,30 @@ namespace Seccion9
 {
   internal class Tarea9
   {
+    static void Main(string[] args)
+    {
+      //Introducir una cadena de caracteres e indicar si es un polindromo. (una palabra polindroma es aquella que se lee igual adelante que atrás) por ejemplo Ana,ojo, rayar, solos, rapar, oso, rotomotor, sedes.
+
+      string palabraUsuario, palabraInvertida = "";
+
+      //Pedimos la palabra
+      Console.Write("Ingresa una palabra y veamos si es un polindromo: ");
+      palabraUsuario = Console.ReadLine();
+
+      foreach (char letra in palabraUsuario)
+      {
+        palabraInvertida = letra + palabraInvertida;
+      }
+
+      if (palabraUsuario.Equals(palabraInvertida, StringComparison.OrdinalIgnoreCase))
+      {
+        Console.WriteLine($"\"{palabraUsuario}\"es una palabrapalindroma");
+      }
+      else
+      {
+        Console.WriteLine($"\"{palabraUsuario}\"No es una palabrapalindroma");
+      }
+    }
     
   }
 } 
